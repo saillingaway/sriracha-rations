190e0fbfa1f9a587a3da352b0c3c57c9fe22e10f
Sophie <seasaltstripes@gmail.com>
2023-07-24 16:40:17 -0400

    updates sriracha-counters to use ngFor button in buttons

--- src/app/sriracha-counters/sriracha-counters.component.ts
+++ src/app/sriracha-counters/sriracha-counters.component.ts
@@ -7,6 +7,13 @@ import { Component } from '@angular/core';
 })
 export class SrirachaCountersComponent {
 
+  buttons = [
+    { size: 'small', oz: 9, count: 0},
+    { size: 'medium', oz: 17, count: 0},
+    { size: 'large', oz: 28, count: 0},
+    { size: 'colossal', oz: 136, count: 0},
+  ];
+
   num_small: number = 0;
   num_medium: number = 0;
   num_large: number = 0;
@@ -15,10 +22,32 @@ export class SrirachaCountersComponent {
   total_oz: number = 0;
   total_lbs: number = 0;
 
-  bottleClicked(amount: number){
-    console.log('bottle with value ', amount, ' was clicked');
+  ngOnInit(){
+    this.buttons.forEach( (button) => {
+      console.log(`${button.size},  ${button.oz}, ${button.count}`);
+    })
+  }
+
+  // bottleClicked(amount: number){
+  //   console.log('bottle with value ', amount, ' was clicked');
     
-    this.updateTotal(amount);
+  //   this.updateTotal(amount);
+  // }
+
+  // onClick(event: any) {
+  //   const button = event.target as HTMLButtonElement;
+  //   console.log(button.value);
+    
+  // }
+
+  onClick(button: any){
+    button.count += 1;
+    console.log(`${button.count},  ${button.oz}`);
+    let old_total = this.total_oz;
+    this.updateTotal(button.oz);
+
+    console.log(`${old_total} + ${button.oz} = ${this.total_oz}`)
+    console.log(`Button ${button.size} clicked. New total: ${this.total_oz}`);
   }
 
   updateTotal(oz: number){
