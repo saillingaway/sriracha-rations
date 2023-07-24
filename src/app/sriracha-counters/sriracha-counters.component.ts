4aeb7627ca8d83d1ad0309a866e7e276e072956d
Sophie <seasaltstripes@gmail.com>
2023-07-24 16:35:01 -0400

    adds bottledClicked(oz) to buttons and calculates total

--- src/app/sriracha-counters/sriracha-counters.component.ts
+++ src/app/sriracha-counters/sriracha-counters.component.ts
@@ -7,7 +7,21 @@ import { Component } from '@angular/core';
 })
 export class SrirachaCountersComponent {
 
-  bottleClicked(){
-    console.log("bottle was clicked");
+  num_small: number = 0;
+  num_medium: number = 0;
+  num_large: number = 0;
+  num_colossal: number = 0;
+
+  total_oz: number = 0;
+  total_lbs: number = 0;
+
+  bottleClicked(amount: number){
+    console.log('bottle with value ', amount, ' was clicked');
+    
+    this.updateTotal(amount);
+  }
+
+  updateTotal(oz: number){
+    this.total_oz += oz;
   }
 }
