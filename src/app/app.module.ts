85596a7ecc971cec9d79d2ed8becfacf2b01ceb4
Sophie <seasaltstripes@gmail.com>
2023-07-24 13:19:11 -0400

    fixes mat-button, centers counter buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -12,7 +13,8 @@ import { SrirachaCountersComponent } from './sriracha-counters/sriracha-counters
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
